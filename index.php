25f049f8c720eb7f3d4b2c530815214ec6154dfd
TARCISIO NUNES <github@tarcnux.com.br>
2019-06-27 21:37:26 -0300

    closes #3 closes #13
    Devido a problema na rede, fois feito commit único

--- index.php
+++ index.php
@@ -9,6 +9,13 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Sistema de Login</title>
+    <style>
+        #alerta,
+        #caixaRegistro,
+        #caixaSenha{
+            display: none;
+        }  
+    </style>
   </head>
   <body class="bg-dark">
       <main class="container mt-4">
@@ -84,7 +91,92 @@
               </div>
           </section>
           <br> <!-- maroto -->
-          
+          <!-- Formulário de Registro de Novo Usuário -->
+          <section class="row">
+              <div class="col-lg-4 offset-lg-4 bg-light rounded" 
+                   id="caixaRegistro">
+                  <h2 class="text-center mt-2">Registrar-se</h2>
+                  <form action="#" method="post" role="form" 
+                        class="p-2" id="formRegistro">
+                      
+                      <!-- Campo Nome Completo -->
+                      <div class="form-group">
+                          <input type="text" name="nomeCompleto" 
+                                 class="form-control"
+                                 placeholder="Nome completo" required
+                                 minlength="6">
+                      </div>
+                      
+                      <!-- Campo Nome do Usuário -->
+                      <div class="form-group">
+                          <input type="text" name="nomeUsuario"
+                                 class="form-control"
+                                 placeholder="Nome do Usuário" required
+                                 minlength="6">
+                      </div>
+                      
+                      <!-- Campo E-mail -->
+                      <div class="form-group">
+                          <input type="email" name="emailUsuario"
+                                 class="form-control"
+                                 placeholder="E-mail" required>
+                      </div>
+                      
+                      <!-- Campo Senha -->
+                      <div class="form-group">
+                          <input type="password" id="senhaUsuario"
+                                 class="form-control"
+                                 placeholder="Senha" required
+                                 minlength="6">
+                      </div>
+                      
+                      <!-- Campo de confirmação de senha -->
+                      <div class="form-group">
+                          <input type="password" 
+                                 id="senhaUsuarioConfirmar" 
+                                 name="senhaUsuarioConfirmar"
+                                 class="form-control"
+                                 placeholder="Confirmar a senha" required
+                                 minlength="6">
+                      </div>
+                      
+                      <!-- Checkbox de aceite dos termos -->
+                      <div class="form-group">
+                          <div class="custom-control custom-checkbox">
+                            <input type="checkbox" name="concordar"
+                                   class="custom-control-input" 
+                                   id="checkConcordar">
+                            <label for="checkConcordar" 
+                                   class="custom-control-label">
+                                Eu concordo com os
+                                <a href="#">termos e condições.</a>
+                            </label>
+                          </div>
+                      </div>
+                      
+                      <!-- botão enviar -->
+                      <div class="form-group">
+                          <input type="submit" 
+                                 name="btnRegistroUsuario"
+                                 id="btnRegistroUsuario"
+                                 value=":: Registrar ::"
+                                 class="btn btn-primary btn-block">
+                      </div>
+                      
+                      <!-- Já registrado -->
+                      <div class="form-group">
+                          <p class="text-center">
+                              Já registrado?
+                              <a href="#" id="btnEntrarRegistrado">
+                                  Entrar aqui.
+                              </a>
+                          </p>
+                      </div>
+                          
+                  </form>                  
+              </div>
+          </section>
+          <br> <!-- maroto -->
           <!-- Formulário de Recuperação de Senha -->
           <section class="row">
               <div class="col-lg-4 offset-lg-4 bg-light rounded"
@@ -133,5 +225,34 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script>
+       //$(document).ready(function(){});
+       //jQuery
+       $(function(){
+           //Mostra caixa de senha no Login
+           $("#btnEsqueci").click(function(){
+               $("#caixaLogin").hide();//Esconder
+               $("#caixaSenha").show();//Mostrar
+           });
+           
+           //Volta para o Login
+           $("#btnVoltar").click(function(){
+               $("#caixaLogin").show();//Mostrar
+               $("#caixaSenha").hide();//Esconder
+           });
+           
+            //Mostra caixa Registrar no Login
+           $("#btnRegistrar").click(function(){
+               $("#caixaLogin").hide();//Esconder
+               $("#caixaRegistro").show();//Mostrar
+           });
+           //Mostra caixa Login no Registro
+           $("#btnEntrarRegistrado").click(function(){
+               $("#caixaLogin").show();//Mostrar
+               $("#caixaRegistro").hide();//Esconder
+           });
+           
+       });
+    </script>
   </body>
 </html>
